622b10806dbe4fded881d96a05f2d3ab94045739
HARSH <103347264+ha-rshh@users.noreply.github.com>
2022-12-20 23:36:26 +0530

    added login and signup script

---
+++ script/signup.js
@@ -0,0 +1,31 @@
+const mobileNum = document.getElementById('mobile');
+const passwordEl = document.getElementById('password');
+const submitBtn = document.getElementById('submit');
+const userNameEl = document.getElementById('username');
+
+let users = JSON.parse(localStorage.getItem('users'))
+
+if(!users){
+    users = [];
+}
+
+function createUser(e){
+    e.preventDefault();
+    const user = {
+        username: userNameEl.value,
+        mobile : mobileNum.value,
+        password: passwordEl.value,
+    }
+    let userFound = false;
+    for(let i = 0; i<users.length; i++){
+        if(user.username === users[i].username || user.mobile === users[i].mobile ){
+            userFound = true;
+            break;
+        }
+    }
+    if(!userFound) users.push(user)
+    else console.log("user already exist")
+
+    localStorage.setItem("users",JSON.stringify(users))
+}
+submitBtn.addEventListener('click', createUser)
