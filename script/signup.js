3008ca4503e215b152c0d95b3dc4b5fce14e733d
HARSH <103347264+ha-rshh@users.noreply.github.com>
2022-12-21 19:04:05 +0530

    signup page done with a resposive design

--- script/signup.js
+++ script/signup.js
@@ -3,7 +3,6 @@ const passwordEl = document.getElementById('password');
 const submitBtn = document.getElementById('submit');
 const userNameEl = document.getElementById('username');
 const loginBtn = document.querySelector('#login-btn');
-
 let users = JSON.parse(localStorage.getItem('users'))
 
 if(!users){
@@ -24,12 +23,13 @@ function createUser(e){
             break;
         }
     }
-    if(!userFound) users.push(user)
+    if(!userFound) users.push(user) 
     else console.log("user already exist")
 
     localStorage.setItem("users",JSON.stringify(users))
 }
 submitBtn.addEventListener('click', createUser)
+
 loginBtn.addEventListener('click', () => {
     window.location.href = "loginpage.html";
 } )
