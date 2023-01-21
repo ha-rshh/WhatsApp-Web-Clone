000084ea1d9f46d188f7db4d53f4d01cc1f9b8ea
Mayur199627 <mayur27asati@gmail.com>
2023-01-22 00:18:50 +0530

    updateAPI

--- script/loginpage.js
+++ script/loginpage.js
@@ -16,40 +16,71 @@ let users = JSON.parse(localStorage.getItem('users'));
 function checkUser(e){
     e.preventDefault();
   
-    if (mobileNum.value == '') {
-        alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E "
-        mobileNumAlert.innerText = "Please enter valid Mobile number"
-        setTimeout(() => {
-            alertDiv.style.cssText = "transition:1s; display: none";
-        }, 2000);
-    } else if (passwordEl.value == '') {
-        alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E "
-        mobileNumAlert.innerText = "Please enter valid Password"
-        setTimeout(() => {
-            alertDiv.style.cssText = "transition:1s; display: none";
-        }, 2000);
-    } else if(!users) {
-        alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E"
-                mobileNumAlert.innerText = "No user found"
-                setTimeout(() => {
-                    alertDiv.style.cssText = "transition:1s; display: none";
-                }, 2000);
-    }
-    for(let i = 0 ; i < users.length; i++){
-        if(mobileNum.value === users[i].mobile && passwordEl.value === users[i].password){
-            window.location.href = "index.html";
-            // console.log('user Exist')
-            const loggedInUser = users[i];
-            localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser))
-        }
-        else {
-            alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E "
+//     if (mobileNum.value == '') {
+//         alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E "
+//         mobileNumAlert.innerText = "Please enter valid Mobile number"
+//         setTimeout(() => {
+//             alertDiv.style.cssText = "transition:1s; display: none";
+//         }, 2000);
+//     } else if (passwordEl.value == '') {
+//         alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E "
+//         mobileNumAlert.innerText = "Please enter valid Password"
+//         setTimeout(() => {
+//             alertDiv.style.cssText = "transition:1s; display: none";
+//         }, 2000);
+//     } else if(!users) {
+//         alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E"
+//                 mobileNumAlert.innerText = "No user found"
+//                 setTimeout(() => {
+//                     alertDiv.style.cssText = "transition:1s; display: none";
+//                 }, 2000);
+//     }
+//     for(let i = 0 ; i < users.length; i++){
+//         if(mobileNum.value === users[i].mobile && passwordEl.value === users[i].password){
+//             window.location.href = "index.html";
+//             // console.log('user Exist')
+//             const loggedInUser = users[i];
+//             localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser))
+//         }
+//         else {
+//             alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E "
+//             mobileNumAlert.innerText = "Your username and/ or password do not match"
+//             setTimeout(() => {
+//                 alertDiv.style.cssText = "transition:1s; display: none";
+//             }, 2000);
+//         }
+//     }
+
+
+let options = {
+    body:JSON.stringify({
+        mobile : mobileNum.value,
+        pass : passwordEl.value
+    }),
+    headers : {
+        "content-type" : "application/json"
+    },
+    method:'POST'
+};
+
+fetch("http://localhost:3000/login", options)
+.then((resolve)=>{
+    return resolve.json();
+})
+.then((data) => {
+    console.log(data)
+    localStorage.setItem("loggedInUser", JSON.stringify(data.data.user));
+    window.location.href = "index.html"
+})
+.catch((error)=>{
+    alert(error);
+    alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E "
             mobileNumAlert.innerText = "Your username and/ or password do not match"
             setTimeout(() => {
                 alertDiv.style.cssText = "transition:1s; display: none";
             }, 2000);
-        }
-    }
+})
+
 }
 
 submitBtn.addEventListener('click', checkUser);
--- script/signup.js
+++ script/signup.js
@@ -6,6 +6,9 @@ const userNameEl = document.getElementById("username");
 const loginBtn = document.querySelector("#login-btn");
 const mobileNumAlert = document.querySelector("#mobile-alert");
 const alertDiv = document.querySelector(".alert");
+const image = document.querySelector("#image");
+
+
 let users = JSON.parse(localStorage.getItem("users"));
 
 if (!users) users = [];
@@ -49,32 +52,69 @@ function createUser(e) {
       alertDiv.style.cssText = "transition:1s; display: none";
     }, 2000);
   } else {
-    for (let i = 0; i < users.length; i++) {
-      if (
-        user.username === users[i].username ||
-        user.mobile === users[i].mobile
-      ) {
-        userFound = true;
-        mobileNumAlert.style.cssText = "transition : 0.8s;";
-        mobileNumAlert.innerText = "User already exist";
-        setTimeout(() => {
-          mobileNumAlert.style.cssText = "transition : 0.8s;";
-          mobileNumAlert.innerText = "Please Log in";
-        }, 1000);
-        break;
-      }
-    }
-    if (!userFound) {
-      users.push(user);
-    } else {
-      alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E";
-      mobileNumAlert.innerText = "User already exist";
-      setTimeout(() => {
-        alertDiv.style.cssText = "transition:1s; display: none";
-      }, 2000);
-    }
-    localStorage.setItem("users", JSON.stringify(users));
-    reset();
+
+    // for (let i = 0; i < users.length; i++) {
+    //   if (
+    //     user.username === users[i].username ||
+    //     user.mobile === users[i].mobile
+    //   ) {
+    //     userFound = true;
+    //     mobileNumAlert.style.cssText = "transition : 0.8s;";
+    //     mobileNumAlert.innerText = "User already exist";
+    //     setTimeout(() => {
+    //       mobileNumAlert.style.cssText = "transition : 0.8s;";
+    //       mobileNumAlert.innerText = "Please Log in";
+    //     }, 1000);
+    //     break;
+    //   }
+    // }
+    // if (!userFound) {
+    //   users.push(user);
+    // } else {
+    //   alertDiv.style.cssText = "transition: 1s; background-color: #FF1E1E";
+    //   mobileNumAlert.innerText = "User already exist";
+    //   setTimeout(() => {
+    //     alertDiv.style.cssText = "transition:1s; display: none";
+    //   }, 2000);
+    // }
+    // localStorage.setItem("users", JSON.stringify(users));
+    // reset();
+
+    let currentDate = new Date();
+    const id = 'id' + performance.now();
+    
+  let options = {
+    body:JSON.stringify({
+      user:userNameEl.value,  
+      mobile : mobileNum.value,
+      pass : passwordEl.value,
+      date:String(currentDate),
+      id,
+      avatar:image.value,
+    }),
+    headers : {
+        "content-type" : "application/json"
+    },
+    method:'POST'
+  };
+
+  fetch("http://localhost:3000", options)
+   .then((resolve) => {
+    localStorage.setItem("loggedInUser", JSON.stringify(resolve.data));
+    window.location.href = "index.html"
+  })
+  .catch((error)=>{
+    alert(error);
+    userFound = true;
+    mobileNumAlert.style.cssText = "transition : 0.8s;";
+    mobileNumAlert.innerText = "User already exist";
+    setTimeout(() => {
+      mobileNumAlert.style.cssText = "transition : 0.8s;";
+      mobileNumAlert.innerText = "Please Log in";
+    }, 1000); 
+  })
+  reset();
+
   }
 }
 
